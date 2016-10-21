3598d09ef181f56300aa4a388246f86f51761420
artbaryl <artbaryl@gmail.com>
2016-10-22 00:03:02 +0200

    Initial commit

---
+++ app/src/main/java/artbaryl/ssep/Intro_Game.java
@@ -0,0 +1,13 @@
+package artbaryl.ssep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Intro_Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro__game);
+    }
+}
