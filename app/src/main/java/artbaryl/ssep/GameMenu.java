b47891445aee51776334a2baf0007ea5897715bd
artbaryl <artbaryl@gmail.com>
2016-11-03 00:56:44 +0100

    Buttons menu upgrade

--- app/src/main/java/artbaryl/ssep/GameMenu.java
+++ app/src/main/java/artbaryl/ssep/GameMenu.java
@@ -1,9 +1,12 @@
 package artbaryl.ssep;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
@@ -24,6 +27,9 @@ public class GameMenu extends AppCompatActivity {
     private LoginButton loginButton;
 
     private CallbackManager callbackManager;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,11 +64,26 @@ public class GameMenu extends AppCompatActivity {
                 info.setText("Login attempt failed.");
             }
         });
+        sharedPreferences = getSharedPreferences("artbaryl.ssep", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
     }
 
     @Override
     protected void onActivityResult(final int requestCode, final int resultCode, final Intent data){
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void Start_game(View view) {
+        if(sharedPreferences.getBoolean("Newgame", false)==false)
+        {
+            editor.putBoolean("Newgame", true);
+            editor.commit();
+        }
+        else
+        {
+            Intent intent = new Intent(this, NewOldGame.class);
+            startActivity(intent);
+        }
     }
 }
